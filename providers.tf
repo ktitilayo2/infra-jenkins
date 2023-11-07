9676bca818056d3f154d994c368e52038d97fa14
ktitilayo2 <126125839+ktitilayo2@users.noreply.github.com>
2023-11-07 16:42:44 -0600

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,21 +1,21 @@
-#terraform {
-#  required_providers {
-#    aws = {
-#      source  = "hashicorp/aws"
-#      version = "~> 5.0"
-#    }
-#  }
-#}
-
-# Configure the AWS Provider
-#provider "aws" {
-#  region     = "us-east-1"
-#  access_key = "AKIA272MK57DH5HGAVWE"
-#  secret_key = "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"
-#}
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-west-2"
+  region     = "us-east-1"
+  access_key = "AKIA272MK57DH5HGAVWE"
+  secret_key = "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"
 }
+
+
+# Configure the AWS Provider
+#provider "aws" {
+#  region = "eu-west-2"
+#}
